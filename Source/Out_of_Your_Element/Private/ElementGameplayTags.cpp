01ca73baaef7d37babbb8398450bfa7549b9c200
Lucas Sivars <48181323+Olivoz@users.noreply.github.com>
2025-11-12 18:35:55 +0100

    Implement basic damage types

---
+++ Source/Out_of_Your_Element/Private/ElementGameplayTags.cpp
@@ -0,0 +1,16 @@
+﻿#include "ElementGameplayTags.h"
+
+namespace ElementGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Damage_Type_Fire, "Damage.Type.Fire", "Fire Damage");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Damage_Type_Water, "Damage.Type.Water", "Water Damage");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Damage_Type_Nature, "Damage.Type.Nature", "Nature Damage");
+
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Abilities_Parameters_Damage,
+	                               "Abilities.Parameters.Damage",
+	                               "Raw damage amount from an attack");
+
+	const FGameplayTagContainer DamageTypes = FGameplayTagContainer::CreateFromArray(TArray<FGameplayTag>{
+		Damage_Type_Fire, Damage_Type_Water, Damage_Type_Nature
+	});
+}
---
+++ Source/Out_of_Your_Element/Public/ElementalDamageEffectExecution.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "GameplayEffectExecutionCalculation.h"
+#include "ElementalDamageEffectExecution.generated.h"
+
+UCLASS()
+class UElementalDamageEffectExecution : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UElementalDamageEffectExecution();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	                                    FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
