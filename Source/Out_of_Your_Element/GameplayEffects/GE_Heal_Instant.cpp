4275019e0e981bfc0349d16fab32065f0c710c0d
Ermias Gonday <erha8436@win.dsv.su.se>
2025-11-19 14:00:06 +0100

    Added GE_Heal_Instant and BP_HealthPickup heal functionality

---
+++ Source/Out_of_Your_Element/GameplayEffects/GE_Heal_Instant.cpp
@@ -0,0 +1,18 @@
+﻿#include "GE_Heal_Instant.h"
+#include "Out_of_Your_Element/AbilitySystem/Attributes/ElementHealthAttributeSet.h"
+
+UGE_Heal_Instant::UGE_Heal_Instant()
+{
+	// This effect applies instantly
+	DurationPolicy = EGameplayEffectDurationType::Instant;
+
+	// Create a modifier that ADDs to Health
+	FGameplayModifierInfo Modifier;
+	Modifier.Attribute = UElementHealthAttributeSet::GetHealthAttribute();
+	Modifier.ModifierOp = EGameplayModOp::Additive;
+
+	// Amount healed → change this number to whatever you want
+	Modifier.ModifierMagnitude = FScalableFloat(20.0f);
+
+	Modifiers.Add(Modifier);
+}
---
+++ Source/Out_of_Your_Element/GameplayEffects/GE_Heal_Instant.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GE_Heal_Instant.generated.h"
+
+UCLASS()
+class OUT_OF_YOUR_ELEMENT_API UGE_Heal_Instant : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+public:
+	UGE_Heal_Instant();
+};
