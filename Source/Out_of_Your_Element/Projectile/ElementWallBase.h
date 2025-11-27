1ffb69f1243f87fdcab6f592fb659541edba5215
ZeroDiego <diegopuentes878@gmail.com>
2025-11-27 23:18:41 +0100

    RockWall WIP

--- Source/Out_of_Your_Element/Projectile/ElementWallBase.h
+++ Source/Out_of_Your_Element/Projectile/ElementWallBase.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "NiagaraSystem.h"
+#include "Abilities/GameplayAbility.h"
 #include "GameFramework/Actor.h"
 #include "ElementWallBase.generated.h"
 
@@ -10,17 +12,40 @@ UCLASS()
 class OUT_OF_YOUR_ELEMENT_API AElementWallBase : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AElementWallBase();
+
+	UPROPERTY(EditAnywhere)
+	FGameplayEffectSpecHandle GameplayEffectSpecHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UGameplayAbility* SourceAbility;
+
+	UPROPERTY(VisibleAnywhere)
+	UNiagaraComponent* WallNiagaraComponent;
+	
+	UPROPERTY(EditAnywhere, Category="VFX")
+	UNiagaraSystem* RockWallPopOutVfx;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	// Called when the game starts or when spawned
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void InitializeZone(const FGameplayEffectSpecHandle& NewGameplayEffectSpecHandle,
+	                    UGameplayAbility* NewSourceAbility, UNiagaraSystem* WallPopInVfx, UNiagaraSystem* WallPopOutVfx,
+	                    const FVector& Scale,
+	                    const FVector& SpawnLocation,
+	                    const float LifeSpan);
 };
