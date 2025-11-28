25f3947e6705d0ca9e1e2c03c9a3c760029ccac5
ZeroDiego <diegopuentes878@gmail.com>
2025-11-28 02:49:57 +0100

    Animation Cooldown Fixes WIP

--- Source/Out_of_Your_Element/Character/ElementCharacter.cpp
+++ Source/Out_of_Your_Element/Character/ElementCharacter.cpp
@@ -15,6 +15,7 @@
 #include "Kismet/KismetMathLibrary.h"
 #include "GameplayAbilitiesModule.h"
 #include "AbilitySystemGlobals.h"
+#include "Out_of_Your_Element/ElementGameplayTags.h"
 
 AElementCharacter::AElementCharacter()
 {
@@ -265,14 +266,12 @@ void AElementCharacter::DoBaseAttack()
 	{
 		if (Tag.IsValid())
 		{
-			if (Tag.GetTagName() == TEXT("Abilities.BaseAttack") || Tag.GetTagName() == TEXT("Abilities.Water"))
+			if (Tag == ElementGameplayTags::Abilities_Water)
 			{
 				return;
 			}
 		}
 	}
-
-	GetCharacterMovement()->MaxWalkSpeed = 150.0f;
 
 	OnAttackDelegate.Broadcast(FAttackData{
 		.Element = ActiveElement,
@@ -282,12 +281,7 @@ void AElementCharacter::DoBaseAttack()
 
 void AElementCharacter::DoBaseAttackHelperFunction(const TSubclassOf<UGameplayAbility>& BaseAttack)
 {
-	if (ElementAbilitySystemComponent->TryActivateAbilityByClass(BaseAttack))
-	{
-		const FGameplayEffectContextHandle AnimationDelayBaseAttackGameplayEffectContextHandle;
-		ElementAbilitySystemComponent->BP_ApplyGameplayEffectToSelf(AnimationDelayBaseAttackGameplayEffect, 1,
-		                                                            AnimationDelayBaseAttackGameplayEffectContextHandle);
-	}
+	ElementAbilitySystemComponent->TryActivateAbilityByClass(BaseAttack);
 }
 
 
@@ -325,20 +319,23 @@ void AElementCharacter::DoSpecialAttack()
 	{
 		if (Tag.IsValid())
 		{
-			if (Tag.GetTagName() == TEXT("Abilities.BaseAttack") || Tag.GetTagName() == TEXT("Abilities.Water"))
+			if (Tag == ElementGameplayTags::Abilities_Water)
 			{
 				return;
 			}
 		}
 	}
 
-	GetCharacterMovement()->MaxWalkSpeed = 150.0f;
-
 	bIsAttacking = true;
 	OnAttackDelegate.Broadcast(FAttackData{
 		.Element = ActiveElement,
 		.Ability = SpecialAttack
 	});
+}
+
+void AElementCharacter::DoSpecialAttackHelperFunction(const TSubclassOf<UGameplayAbility>& SpecialAttack)
+{
+	ElementAbilitySystemComponent->TryActivateAbilityByClass(SpecialAttack);
 }
 
 void AElementCharacter::DoCycleElement(const int Amount)
