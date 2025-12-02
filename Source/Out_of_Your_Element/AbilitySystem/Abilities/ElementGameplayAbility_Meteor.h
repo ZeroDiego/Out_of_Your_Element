35eaa2bc218e669acd57630d5c5e1bf48eccc21d
Lucas Sivars <48181323+Olivoz@users.noreply.github.com>
2025-12-02 19:58:54 +0100

    Meteor Projectile

--- Source/Out_of_Your_Element/AbilitySystem/Abilities/ElementGameplayAbility_Meteor.h
+++ Source/Out_of_Your_Element/AbilitySystem/Abilities/ElementGameplayAbility_Meteor.h
@@ -7,9 +7,6 @@
 #include "Out_of_Your_Element/Projectile/ElementZoneBase.h"
 #include "ElementGameplayAbility_Meteor.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class OUT_OF_YOUR_ELEMENT_API UElementGameplayAbility_Meteor : public UElementGameplayAbilityBase
 {
@@ -17,16 +14,22 @@ class OUT_OF_YOUR_ELEMENT_API UElementGameplayAbility_Meteor : public UElementGa
 
 public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-								 const FGameplayAbilityActorInfo* ActorInfo,
-								 const FGameplayAbilityActivationInfo ActivationInfo,
-								 const FGameplayEventData* TriggerEventData) override;
+	                             const FGameplayAbilityActorInfo* ActorInfo,
+	                             const FGameplayAbilityActivationInfo ActivationInfo,
+	                             const FGameplayEventData* TriggerEventData) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	FVector MeteorSpawnOffset = FVector(0, 0, 5000);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TSubclassOf<class AElementMeteor> MeteorClass;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
 	TSubclassOf<AElementZoneBase> MeteorZoneClass;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta=(Units="Seconds"), Category="Meteor")
 	float MeteorSummoningTime = 3;
-	
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta=(Units="Seconds"), Category="Meteor")
 	float DotDamageDuration = 4;
 
