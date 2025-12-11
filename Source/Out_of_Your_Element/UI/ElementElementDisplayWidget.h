c9d6bac6329b8973d44372e28fa1b9ad4834163f
Lucas Sivars <48181323+Olivoz@users.noreply.github.com>
2025-12-11 13:46:06 +0100

    Redo element wheel implementation

---
+++ Source/Out_of_Your_Element/UI/ElementElementDisplayWidget.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "ElementElementDisplayWidget.generated.h"
+
+UCLASS(Abstract, Blueprintable)
+class OUT_OF_YOUR_ELEMENT_API UElementElementDisplayWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess=true))
+	bool bIsSpinning = false;
+
+	float DegreesToSpin;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetTargetRotation(const float Degrees);
+
+	UFUNCTION(BlueprintCallable)
+	void Rotate(const float Degrees);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	float GetCurrentRotation();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void SetCurrentRotation(const float NewRotation);
+
+protected:
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	bool RotateTowardsTarget(float Speed, float DeltaTime);
+};
